6549ef983a1bef0910f87ee2828f8762ca3f1f82
Alexandre <alexandrenunes1331@gmail.com>
2016-12-29 23:20:53 +0000

    First Commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<HTML>
+	<HEAD>
+		<TITLE>CodeDB</TITLE>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel='shortcut icon' type='image/x-icon' href='favicon.ico' />
+	</HEAD>
+	<BODY>
+		<center>
+			<img src="img/logo.png" width="40%"><br><br><br>
+				<form method="post" action="search.php">
+				<input type="text" name="query">
+				<select name="language">
+					<?php
+						$languages=scandir("code");
+						foreach($languages as $language){
+							if($language!="." and $language!=".."){
+								echo "<option value='".$language."'>".$language."</option>";
+							}
+						}
+					?>
+				</select>
+				<button type="submit">Find</button>
+			</form><br>
+			<hr>
+			<a href="addcode.php">Click</a> to add code to DataBase
+		</center>
+	</BODY>
+</HTML>
